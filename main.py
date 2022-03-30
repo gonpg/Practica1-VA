89b41fc16553c2ccc02ec14c1d2190735ce478e5
Michael <mr.toala.2017@alumnos.urjc.es>
2022-03-30 15:59:59 +0200

    cambio2

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import argparse
+import os
+import cv2
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-#hoioloa
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def loadTrainData(path):
+    imgs = os.listdir(path)
+    for i in imgs:
+        if os.path.isdir(path + "/" + i):
+            print("error")
+        else:
+            print(i)
+            img = cv2.imread(path + '/' + str(i), 0)
+            plt.imshow(img, cmap="gray")
+            plt.show()
+            # hist_item = cv2.calcHist([img],[0],None,[256],[0,255])
+            # plt.plot(hist_item.reshape(256,))
+
+
+def main():
+    loadTrainData(args.train_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Trains and executes a given detector over a set of testing images')
+    parser.add_argument(
+        '--detector', type=str, nargs="?", default="", help='Detector string name')
+    parser.add_argument(
+        '--train_path', default="", help='Select the training data dir')
+    parser.add_argument(
+        '--test_path', default="", help='Select the testing data dir')
+
+    args = parser.parse_args()
+
+    main()
+
+    # Load training data
+
+    # Create the detector
+
+    # Load testing data
+
+    # Evaluate sign detections
+
+# main.py --detector --train_path train_jpg --test_path test_alumnos_jpg
