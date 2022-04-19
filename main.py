ae50b0a66932ad2b832428ed8d2565f67f0d1c35
Michael <mr.toala.2017@alumnos.urjc.es>
2022-04-19 23:33:55 +0200

    mser: aproximación nº 1

--- main.py
+++ main.py
@@ -4,13 +4,47 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def main():
+    cargarMascaras(args.train_path)
+    detector(args.test_path)  # hacer comparacion, calculo regiones, etc
+
+
+def cargarMascaras(path):
+    try:
+        os.makedirs('mascaras')
+    except FileExistsError:
+        print("Ya existe el directorio")
+        # pass
+
+    prohibicion = ["00", "01", "02", "03", "04", "05", "07", "08", "09", "10", "15", "16"]
+    resultado = HSV(media(path, prohibicion), 1)
+    cv2.imwrite("mascaras/prohibicion.jpg", resultado)
+
+    peligro = ["11", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
+    resultado = HSV(media(path, peligro), 1)
+    cv2.imwrite("mascaras/peligro.jpg", resultado)
+
+    dirProhibida = ["17"]
+    resultado = HSV(media(path, dirProhibida), 1)
+    cv2.imwrite("mascaras/dirProhibida.jpg", resultado)
+
+    cedaPaso = ["13"]
+    resultado = HSV(media(path, cedaPaso), 1)
+    cv2.imwrite("mascaras/cedaPaso.jpg", resultado)
+
+    dirObligatoria = ["38"]
+    resultado = HSV(media(path, dirObligatoria), 2)
+    cv2.imwrite("mascaras/dirObligatoria.jpg", resultado)
+
+
 def media(path, nombres):
     suma = cv2.imread(path + "/" + nombres[0] + "/00000.jpg", 1)
     k = 0
     for i in nombres:
         direccion = path + "/" + i
-        imgs = os.listdir(direccion)
-        for j in imgs:
+        imagenes = os.listdir(direccion)
+        for j in imagenes:
             ## https://stackoverflow.com/questions/57723968/blending-multiple-images-with-opencv ##
             img = cv2.imread(direccion + "/" + j, 1)  # imagen en BGR
             img = cv2.resize(img, (suma.shape[1], suma.shape[0]), interpolation=cv2.INTER_CUBIC)
@@ -22,6 +56,7 @@ def media(path, nombres):
 
     return suma
 
+
 def HSV(img, mode):
     img = cv2.resize(img, (25, 25))
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -31,8 +66,6 @@ def HSV(img, mode):
         lowBlue = np.array([90, 70, 50], np.uint8)
         highBlue = np.array([128, 255, 255], np.uint8)
         maskBlue = cv2.inRange(hsv, lowBlue, highBlue)
-        plt.imshow(maskBlue)
-        plt.show()
         ## - ##
 
         return maskBlue
@@ -51,30 +84,25 @@ def HSV(img, mode):
     maskRed = cv2.add(mask1, mask2)
     return maskRed
 
-def cargarMascaras():
-    prohibicion = ["00", "01", "02", "03", "04", "05", "07", "08", "09", "10", "15", "16"]
-    resultado = HSV(media(args.train_path, prohibicion), 1)
-    cv2.imwrite("prohibicion.jpg", resultado)
 
-    peligro = ["11", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-    resultado = HSV(media(args.train_path, peligro), 1)
-    cv2.imwrite("peligro.jpg", resultado)
+def detector(path):
+    imagenes = os.listdir(path)
+    direccion = path + "/"
+    for img in imagenes:
+        imgGray = cv2.imread(direccion + img, 1)
+        imgOriginal = cv2.imread(direccion + img, cv2.IMREAD_COLOR)
+        imgGray = cv2.cvtColor(imgGray, cv2.COLOR_BGR2GRAY)
+        equ_image = cv2.equalizeHist(imgGray)
 
-    dirProhibida = ["17"]
-    resultado = HSV(media(args.train_path, dirProhibida), 1)
-    cv2.imwrite("dirProhibida.jpg", resultado)
+        # plt.imshow(cv2.cvtColor(imgGray, cv2.COLOR_BGR2RGB))
+        # plt.show()
 
-    cedaPaso = ["13"]
-    resultado = HSV(media(args.train_path, cedaPaso), 1)
-    cv2.imwrite("cedaPaso.jpg", resultado)
+        # plt.imshow(equ_image,cmap="gray",vmin=0,vmax=255)
+        # plt.show()
 
-    dirObligatoria = ["38"]
-    resultado = HSV(media(args.train_path, dirObligatoria), 2)
-    cv2.imwrite("dirObligatoria.jpg", resultado)
+        mser = cv2.MSER_create()
+        regions, _ = mser.detectRegions(equ_image)
 
-def main():
-    cargarMascaras()
-    # hacer comparacion, calculo regiones, etc
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
